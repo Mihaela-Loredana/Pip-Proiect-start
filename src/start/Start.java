fc5edc3fba2397a3df44f9b31a0e8727476d5f7d
Stefan <Stefan@DESKTOP-92FHH4N>
2022-03-09 01:16:25 +0200

    am adaugat al doilea comit

--- src/start/Start.java
+++ src/start/Start.java
@@ -5,6 +5,7 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Salut, aceasta este prima afisare");
+		System.out.println("Acesta este a doua afisare, dupa ce am dat push");
 
 	}
 
