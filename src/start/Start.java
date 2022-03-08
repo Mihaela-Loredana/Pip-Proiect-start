930bec919eaaaf6037065291797527eacbb7459a
Mihaela-Loredana Ignat <mihaela-loredana.ignat@student.tuiasi.ro>
2022-03-09 01:23:14 +0200

    Commit by Loredana

--- src/start/Start.java
+++ src/start/Start.java
@@ -6,7 +6,7 @@ public class Start {
 		// TODO Auto-generated method stub
 		System.out.println("Salut, aceasta este prima afisare");
 		System.out.println("Acesta este a doua afisare, dupa ce am dat push");
-
+        System.out.println("Primul commit - Loredana");
 	}
 
 }
